14e1bb3a1afe2b3fecc932d9912f95cad6e3ec05
AnastasiaGrigoryeva <nastya02011995@gmail.com>
2023-02-07 00:04:20 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,166 @@
+        function myFunc(id) {
+            document.querySelector(".s5-btn").classList.toggle("clicked")
+        }
+
+
+        let nav = document.getElementById("nav-button")
+        function myFuncNav() {
+        document.getElementById("nav-toggle").classList.toggle("show1")
+        document.querySelector(".img.lang.one").classList.toggle("show2")
+        }
+        nav.addEventListener("click", myFuncNav);
+        
+        let btn1 = document.getElementById("btn")
+        function myFunction() {
+            document.getElementById("toggle").classList.toggle("show")
+            document.querySelector(".s5-box.one").classList.toggle("focused") 
+            document.querySelector(".s5-box.title.one").classList.toggle("chosen")
+            btn.classList.toggle("clicked")
+        }
+        btn1.addEventListener("click", myFunction);
+
+
+        let btn2 = document.getElementById("btn-2")
+        function myFunction2() {
+            document.getElementById("toggle-2").classList.toggle("show")
+            document.querySelector(".s5-box.two").classList.toggle("focused") 
+            document.querySelector(".s5-box.title.two").classList.toggle("chosen")
+            btn2.classList.toggle("clicked")
+        }
+        btn2.addEventListener("click", myFunction2);
+
+        let btn3 = document.getElementById("btn-3")
+        function myFunction3() {
+        document.getElementById("toggle-3").classList.toggle("show")
+        document.querySelector(".s5-box.three").classList.toggle("focused")
+        document.querySelector(".s5-box.title.three").classList.toggle("chosen")
+        btn3.classList.toggle("clicked")
+        }
+        btn3.addEventListener("click", myFunction3);
+
+        let btn4 = document.getElementById("btn-4")
+        function myFunction4() {
+        document.getElementById("toggle-4").classList.toggle("show")
+        document.querySelector(".s5-box.four").classList.toggle("focused")
+        document.querySelector(".s5-box.title.four").classList.toggle("chosen")
+        btn4.classList.toggle("clicked")
+        }
+        btn4.addEventListener("click", myFunction4);
+
+        let btn5 = document.getElementById("btn-5")
+        function myFunction5() {
+        document.getElementById("toggle-5").classList.toggle("show")
+        document.querySelector(".s5-box.five").classList.toggle("focused")
+        document.querySelector(".s5-box.title.five").classList.toggle("chosen")
+        btn5.classList.toggle("clicked")
+        }
+        btn5.addEventListener("click", myFunction5);
+
+        let btn6 = document.getElementById("btn-6")
+        function myFunction6() {
+        document.getElementById("toggle-6").classList.toggle("show")
+        document.querySelector(".s5-box.six").classList.toggle("focused")
+        document.querySelector(".s5-box.title.six").classList.toggle("chosen")
+        btn6.classList.toggle("clicked")
+        }
+        btn6.addEventListener("click", myFunction6);
+
+        let btn7 = document.getElementById("btn-7")
+        function myFunction7() {
+        document.getElementById("toggle-7").classList.toggle("show")
+        document.querySelector(".s5-box.seven").classList.toggle("focused")
+        document.querySelector(".s5-box.title.seven").classList.toggle("chosen")
+        btn7.classList.toggle("clicked")
+        }
+        btn7.addEventListener("click", myFunction7);
+
+        let btn8 = document.getElementById("btn-8")
+        function myFunction8() {
+        document.getElementById("toggle-8").classList.toggle("show")
+        document.querySelector(".s5-box.eight").classList.toggle("focused")
+        document.querySelector(".s5-box.title.eight").classList.toggle("chosen")
+        btn8.classList.toggle("clicked")
+        }
+        btn8.addEventListener("click", myFunction8);
+
+        let btn9 = document.getElementById("btn-9")
+        function myFunction9() {
+        document.getElementById("toggle-9").classList.toggle("show")
+        document.querySelector(".s5-box.nine").classList.toggle("focused")
+        document.querySelector(".s5-box.title.nine").classList.toggle("chosen")
+        btn9.classList.toggle("clicked")
+        }
+        btn9.addEventListener("click", myFunction9);
+
+        let btn10 = document.getElementById("btn-10")
+        function myFunction10() {
+        document.getElementById("toggle-10").classList.toggle("show")
+        document.querySelector(".s5-box.ten").classList.toggle("focused")
+        document.querySelector(".s5-box.title.ten").classList.toggle("chosen")
+        btn10.classList.toggle("clicked")
+        }
+        btn10.addEventListener("click", myFunction10);
+
+        let btn11 = document.getElementById("btn-11")
+        function myFunction11() {
+        document.getElementById("toggle-11").classList.toggle("show")
+        document.querySelector(".s5-box.eleven").classList.toggle("focused")
+        document.querySelector(".s5-box.title.eleven").classList.toggle("chosen")
+        btn11.classList.toggle("clicked")
+        }
+        btn11.addEventListener("click", myFunction11);
+
+        let btn12 = document.getElementById("btn-12")
+        function myFunction12() {
+        document.getElementById("toggle-12").classList.toggle("show")
+        document.querySelector(".s5-box.tvelve").classList.toggle("focused")
+        document.querySelector(".s5-box.title.tvelve").classList.toggle("chosen")
+        btn12.classList.toggle("clicked")
+        }
+        btn12.addEventListener("click", myFunction12);
+
+        let btn13 = document.getElementById("btn-13")
+        function myFunction13() {
+        document.getElementById("toggle-13").classList.toggle("show")
+        document.querySelector(".s8-box.one").classList.toggle("chosen")
+        document.querySelector(".s8-box.title.one").classList.toggle("chosen")
+        btn13.classList.toggle("clicked")
+        }
+        btn13.addEventListener("click", myFunction13);
+
+        let btn14 = document.getElementById("btn-14")
+        function myFunction14() {
+        document.getElementById("toggle-14").classList.toggle("show")
+        document.querySelector(".s8-box.two").classList.toggle("chosen")
+        document.querySelector(".s8-box.title.two").classList.toggle("chosen")
+        btn14.classList.toggle("clicked")
+        }
+        btn14.addEventListener("click", myFunction14);
+
+        let btn15 = document.getElementById("btn-15")
+        function myFunction15() {
+        document.getElementById("toggle-15").classList.toggle("show")
+        document.querySelector(".s8-box.three").classList.toggle("chosen")
+        document.querySelector(".s8-box.title.three").classList.toggle("chosen")
+        btn15.classList.toggle("clicked")
+        }
+        btn15.addEventListener("click", myFunction15);
+
+        let btn16 = document.getElementById("btn-16")
+        function myFunction16() {
+        document.getElementById("toggle-16").classList.toggle("show")
+        document.querySelector(".s8-box.four").classList.toggle("chosen")
+        document.querySelector(".s8-box.title.four").classList.toggle("chosen")
+        btn16.classList.toggle("clicked")
+        }
+        btn16.addEventListener("click", myFunction16);
+
+        let btn17 = document.getElementById("btn-17")
+        function myFunction17() {
+        document.getElementById("toggle-17").classList.toggle("show")
+        document.querySelector(".s8-box.five").classList.toggle("chosen")
+        document.querySelector(".s8-box.title.five").classList.toggle("chosen")
+        btn17.classList.toggle("clicked")
+        }
+        btn17.addEventListener("click", myFunction17);
+
